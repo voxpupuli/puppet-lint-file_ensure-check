3edf8d3c83352f8813688c72e210c0060da93de7
Raphaël Pinson <raphael.pinson@camptocamp.com>
2015-02-10 11:21:26 +0100

    Add fix

--- lib/puppet-lint/plugins/check_file_ensure.rb
+++ lib/puppet-lint/plugins/check_file_ensure.rb
@@ -2,19 +2,33 @@ PuppetLint.new_check(:file_ensure) do
   def check
     resource_indexes.each do |resource|
       if resource[:type].value == 'file'
-        attr = resource[:tokens].select { |t| t.type == :NAME && t.value == 'ensure' && t.next_code_token.type == :FARROW }
+        attr = resource[:tokens].select { |t| t.type == :NAME && \
+                                              t.value == 'ensure' && \
+                                              t.next_code_token.type == :FARROW }
         unless attr.empty?
           val_token = attr[0].next_code_token.next_code_token
           if val_token.value == 'present'
               notify :warning, {
-                :message => 'ensure set to present on file resource',
-                :line    => val_token.line,
-                :column  => val_token.column,
-                :token   => val_token,
+                :message  => 'ensure set to present on file resource',
+                :line     => val_token.line,
+                :column   => val_token.column,
+                :token    => val_token,
+                :resource => resource,
               }
           end
         end
       end
+    end
+  end
+
+  def fix(problem)
+    target_attr = problem[:resource][:tokens].select { |t| t.type == :NAME && \
+                                                           t.value == 'target' && \
+                                                           t.next_code_token.type == :FARROW }
+    if target_attr.empty?
+      problem[:token].value = 'file'
+    else
+      problem[:token].value = 'link'
     end
   end
 end
--- spec/puppet-lint/plugins/check_file_ensure/check_file_ensure_spec.rb
+++ spec/puppet-lint/plugins/check_file_ensure/check_file_ensure_spec.rb
@@ -12,7 +12,8 @@ describe 'file_ensure' do
         }
 
         file { '/etc/fstab':
-          ensure => 'file',
+          ensure => 'target',
+          target => '/etc/mtab',
         }
         EOS
       }
@@ -31,17 +32,90 @@ describe 'file_ensure' do
 
         file { '/etc/fstab':
           ensure => 'present',
+          target => '/etc/mtab',
         }
         EOS
       }
 
-      it 'should detect a single problem' do
+      it 'should detect problems' do
         expect(problems).to have(2).problems
       end
 
       it 'should create a warning' do
         expect(problems).to contain_warning(msg).on_line(2).in_column(21)
         expect(problems).to contain_warning(msg).on_line(6).in_column(21)
+      end
+    end
+  end
+
+  context 'with fix enabled' do
+    before do
+      PuppetLint.configuration.fix = true
+    end
+
+    after do
+      PuppetLint.configuration.fix = false
+    end
+
+    context 'correct file resource declarations' do
+      let (:code) {
+        <<-EOS
+        file { '/etc/sudoers':
+          ensure => file,
+        }
+
+        file { '/etc/fstab':
+          ensure => 'target',
+          target => '/etc/mtab',
+        }
+        EOS
+      }
+
+      it 'should not detect any problems' do
+        expect(problems).to have(0).problems
+      end
+
+      it 'should not modify the manifest' do
+        expect(manifest).to eq(code)
+      end
+    end
+
+    context 'wrong file resource declarations' do
+      let (:code) {
+        <<-EOS
+        file { '/etc/sudoers':
+          ensure => present,
+        }
+
+        file { '/etc/fstab':
+          ensure => 'present',
+          target => '/etc/mtab',
+        }
+        EOS
+      }
+
+      it 'should detect two problems' do
+        expect(problems).to have(2).problems
+      end
+
+      it 'should create a warning' do
+        expect(problems).to contain_fixed(msg).on_line(2).in_column(21)
+        expect(problems).to contain_fixed(msg).on_line(6).in_column(21)
+      end
+
+      it 'should fix the ensure parameter' do
+        expect(manifest).to eq(
+          <<-EOS
+        file { '/etc/sudoers':
+          ensure => file,
+        }
+
+        file { '/etc/fstab':
+          ensure => 'link',
+          target => '/etc/mtab',
+        }
+          EOS
+        )
       end
     end
   end
